515c86afd14e202ca5537fe703a584d80412890d
Alan-Liang <a@keeer.net>
2019-07-02 20:40:51 +0800

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,20 @@
+var cacheName = 'Scratchpad-v0.0.1'
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(['/', '/MarkdownIME.min.js'])
+    })
+  )
+})
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          cache.put(e.request, response.clone())
+          return response
+        })
+      })
+    })
+  )
+})
